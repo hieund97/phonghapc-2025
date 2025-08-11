b2acd20aabb15a18f03b8cb29e353e450d06469f
Nguyen Duc Hieu <duchieu97.hn@gmail.com>
2025-08-11 23:50:22 +0700

    fix css suggest

--- resources/views/front_end/searchs/suggest.blade.php
+++ resources/views/front_end/searchs/suggest.blade.php
@@ -18,7 +18,7 @@
                                 {{ $product->name }}
                             </a>
                         </h2>
-                        <div class="price">
+                        <div class="price" style="width:10%">
                             <div class="new">@money($product->getRealPriceAttribute())</div>
                             @if(!empty($product->sale_price))
                                 <div class="old">@money($product->price)</div>
