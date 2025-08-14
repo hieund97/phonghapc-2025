546ac97e413b242de60ce6eb599c7c8cb966ebc6
Nguyen Duc Hieu <duchieu97.hn@gmail.com>
2025-08-14 15:56:53 +0700

    update css

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,20 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(RolesAndPermissionsSeeder::class);
-        //$this->call(OptionsTableSeeder::class);
-        //$this->call(FooterLinkSeeder::class);
+        $this->call(OptionsTableSeeder::class);
+        $this->call(FooterLinkSeeder::class);
         //$this->call(CategoriesTableSeeder::class);
         //$this->call(ProductCategorySeeder::class);
         //$this->call(MediaFolderSeeder::class);
         //$this->call(ProductsTableSeeder::class);
         //$this->call(BranchSeeder::class);
-        //$this->call(ProvincesTableSeeder::class);
-        //$this->call(DistrictsTableSeeder::class);
-        //$this->call(SliderSeeder::class);
-        //$this->call(\Database\Seeders\MenuSeeder::class);
-        //$this->call(\Database\Seeders\MenuItemSeeder::class);
-        //$this->call(\Database\Seeders\ContactSeeder::class);
+        $this->call(ProvincesTableSeeder::class);
+        $this->call(DistrictsTableSeeder::class);
+        $this->call(SliderSeeder::class);
+        $this->call(\Database\Seeders\MenuSeeder::class);
+        $this->call(\Database\Seeders\MenuItemSeeder::class);
+        $this->call(\Database\Seeders\ContactSeeder::class);
         //$this->call(\Database\Seeders\PostSeeder::class);
-        //$this->call(WardsTableSeeder::class);
+        $this->call(WardsTableSeeder::class);
     }
 }
