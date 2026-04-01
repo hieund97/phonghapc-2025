207778330b2a5b5e1adbae544e3674355fc87733
hieund-geniee <hieund@geniee.co.jp>
2026-04-01 10:49:09 +0700

    add maintance mode

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode as Middleware;
+use Closure;
+use Illuminate\Http\Request;
 
-class CheckForMaintenanceMode extends Middleware
+class CheckForMaintenanceMode
 {
     /**
      * The URIs that should be reachable while maintenance mode is enabled.
@@ -12,6 +13,40 @@ class CheckForMaintenanceMode extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'ph_admin',
+        'ph_admin/*',
     ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (env('MAINTENANCE_MODE', false) && !$this->isExcluded($request)) {
+            return response()->file(public_path('maintenance.html'), 503);
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * Check if the request URI is excluded from maintenance mode.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function isExcluded(Request $request): bool
+    {
+        foreach ($this->except as $except) {
+            if ($request->is($except)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
